071971cb875a3dca86b224e519a863ef99730fbf
PicToWeb <picronantoine34@gmail.com>
2024-06-10 11:40:22 +0200

    correction ex01 - tp 03

--- src/test/java/java17/ex01/Function_01_Test.java
+++ src/test/java/java17/ex01/Function_01_Test.java
@@ -7,69 +7,60 @@ import org.junit.Test;
 import java17.data.Account;
 import java17.data.Person;
 
-
 /**
  * Exercice 01 - java.util.function.Function
  */
 public class Function_01_Test {
 
-    /******** PART 1 - Integer -> Person *******/
+	/******** PART 1 - Integer -> Person *******/
 
-    // tag::intToPerson[]
-    // TODO Compléter la définition de cette fonction
-    // TODO Cette fonction permet de transformer un entier en objet Person
-    // TODO le prenom sera de la forme "first_<ENTIER>"
-    // TODO le nom sera de la forme "last_<ENTIER>"
-    // TODO l'age sera de la forme "<ENTIER>"
-    // TODO le mot de passe sera de la forme "pass_<ENTIER>"
-    private Function<Integer, Person> intToPerson = entier -> {
-    	String firstName = "first_" + entier;
-    	String lastName = "last_" + entier;
-    	String age = "" + entier;
-    	String pass = "pass_" + entier;
-    	
-    	return new Person(firstName,lastName,Integer.parseInt(age),pass);
-    };
-    // end::intToPerson[]
+	// tag::intToPerson[]
+	// TODO Compléter la définition de cette fonction
+	// TODO Cette fonction permet de transformer un entier en objet Person
+	// TODO le prenom sera de la forme "first_<ENTIER>"
+	// TODO le nom sera de la forme "last_<ENTIER>"
+	// TODO l'age sera de la forme "<ENTIER>"
+	// TODO le mot de passe sera de la forme "pass_<ENTIER>"
+	private Function<Integer, Person> intToPerson = entier -> new Person("first_" + entier, "last_" + entier, entier,
+			"pass_" + entier);
 
-    @Test
-    public void test_intToPerson() throws Exception {
+	// end::intToPerson[]
 
-        // TODO invoquer la fonction intToPerson avec en paramètre l'entier 10.
-        Person result = intToPerson.apply(10);
+	@Test
+	public void test_intToPerson() throws Exception {
 
-        assert result.getFirstname().equals("first_10");
-        assert result.getLastname().equals("last_10");
-        assert result.getAge().equals(10);
-        assert result.getPassword().equals("pass_10");
-    }
+		// TODO invoquer la fonction intToPerson avec en paramètre l'entier 10.
+		Person result = intToPerson.apply(10);
 
-    /******** PART 2 - Person -> Account *******/
+		assert result.getFirstname().equals("first_10");
+		assert result.getLastname().equals("last_10");
+		assert result.getAge().equals(10);
+		assert result.getPassword().equals("pass_10");
+	}
 
-    // tag::personToAccount[]
-    // TODO Compléter la définition de cette fonction
-    // TODO la propriété owner est valorisé avec la personne en paramètre
-    // TODO la propriété balance est valorisé à 1000
-    private Function<Person, Account> personToAccount = person ->{
-    	    	
-    	Account acc = new Account();
-    	acc.setOwner(person);
-    	acc.setBalance(1000);
-    	
-    	return acc;
-    	
-    };
-    // end::personToAccount[]
+	/******** PART 2 - Person -> Account *******/
 
-    @Test
-    public void test_personToAccount() throws Exception {
+	// tag::personToAccount[]
+	// TODO Compléter la définition de cette fonction
+	// TODO la propriété owner est valorisé avec la personne en paramètre
+	// TODO la propriété balance est valorisé à 1000
+	private Function<Person, Account> personToAccount = person -> {
+		Account acc = new Account();
+		acc.setOwner(person);
+		acc.setBalance(1000);
+		return acc;
+	};
+	// end::personToAccount[]
 
-        Person person = new Person("Jules", "France", 10, "pass");
+	@Test
+	public void test_personToAccount() throws Exception {
 
-        // TODO invoquer la fonction personToAccount
-        Account result = personToAccount.apply(person);
+		Person person = new Person("Jules", "France", 10, "pass");
 
-        assert result.getOwner().equals(person);
-        assert result.getBalance().equals(1000);
-    }
+		// TODO invoquer la fonction personToAccount
+		Account result = personToAccount.apply(person);
+
+		assert result.getOwner().equals(person);
+		assert result.getBalance().equals(1000);
+	}
 }
