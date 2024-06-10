0d258492aee0c8cd2e7546b4efc0f2d6a1c2e673
PicToWeb <picronantoine34@gmail.com>
2024-06-10 14:39:41 +0200

    ex05

--- src/test/java/java8/ex05/Stream_05_Test.java
+++ src/test/java/java8/ex05/Stream_05_Test.java
@@ -34,8 +34,9 @@ public class Stream_05_Test {
     // TODO utiliser la méthode Stream.iterate
     // TODO cette méthode doit produire le même résultat que imperativeSum
     private long iterateSum(long n) {
+    	
         return 0;
-    }
+    } 
 
     // TODO exécuter le test pour vérifier que les méthodes imperativeSum et iterateSum produisent le même résultat
     @Test
