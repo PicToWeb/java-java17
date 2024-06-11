993303c07fd500733ece2335f332d1ed1eeeb24f
PicToWeb <picronantoine34@gmail.com>
2024-06-11 12:29:32 +0200

    date, ZoneId,LocalDate

--- src/test/java/java17/ex07/DateAndTime_07_Test.java
+++ src/test/java/java17/ex07/DateAndTime_07_Test.java
@@ -3,8 +3,10 @@ package java17.ex07;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.ZoneId;
 import java.util.Date;
 
 import org.junit.Test;
@@ -18,16 +20,20 @@ public class DateAndTime_07_Test {
 	public void test_date_to_localdate() throws Exception {
 
 		// TODO Créer une date Java 1 (12/02/2017)
-		Date date = null;
+		Date date = new Date(117,01,12);
 
 		// TODO transformer la date en Instant
+		Instant date1 = date.toInstant();
 
 		// TODO transformer la date en LocalDate
-
-		LocalDate result = null;
+		ZoneId zone = ZoneId.systemDefault();
+		LocalDate result = LocalDate.ofInstant(date1, zone);
 
 		// TODO valoriser les différentes variables afin de rendre le test passant
-
+		int year = result.getYear();
+		Month month = result.getMonth();
+		int day = result.getDayOfMonth();
+		
 		assertThat(result.getYear(), is(2017));
 		assertThat(result.getMonth(), is(Month.FEBRUARY));
 		assertThat(result.getDayOfMonth(), is(12));
